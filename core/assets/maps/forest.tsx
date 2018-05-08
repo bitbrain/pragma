268f219a27b13f421c93e3449233e38394093ef7
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2018-05-08 13:25:46 +0100

    Add car

--- core/assets/maps/forest.tsx
+++ core/assets/maps/forest.tsx
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
-<tileset name="forest" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="forest.png" width="196" height="128"/>
- <tile id="6">
+<tileset name="forest" tilewidth="32" tileheight="32" tilecount="40" columns="10">
+ <image source="forest.png" width="320" height="128"/>
+ <tile id="10">
   <properties>
    <property name="collision" value="false"/>
   </properties>
  </tile>
- <tile id="7">
+ <tile id="11">
   <properties>
    <property name="collision" value="false"/>
   </properties>
  </tile>
- <tile id="8">
+ <tile id="12">
   <properties>
    <property name="collision" value="true"/>
   </properties>
@@ -23,6 +23,21 @@
   </properties>
  </tile>
  <tile id="17">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25">
   <properties>
    <property name="collision" value="true"/>
   </properties>
