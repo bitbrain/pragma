35ff148172ffb8607184277c16c0790a46196345
Miguel Gonzalez Sanchez <miguel-gonzalez@gmx.de>
2018-05-17 18:44:52 +0100

    Fix build

--- deploy-desktop.sh
+++ deploy-desktop.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 echo "Pack game.."
-mkdir game
+
 mv desktop/build/libs/desktop-1.0.jar desktop/build/libs/pragma.jar
 
 echo "Deploying to itch.io.."
 wget http://dl.itch.ovh/butler/linux-amd64/head/butler
+
 chmod +x butler
-touch butler_creds
-echo -n $ITCH_API_KEY > butler_creds
 
 # Upload game
-./butler push pragma.jar bitbrain/pragma:windows-linux-mac -i butler_creds
+./butler push desktop/build/libs/pragma.jar bitbrain/pragma:windows-linux-mac
 
 # Cleanup
 echo "Cleanup.."
 ./butler logout -i butler_creds --assume-yes
-rm -rf pragma.jar
+rm -rf desktop/build
 rm butler
 
 echo "Done."
